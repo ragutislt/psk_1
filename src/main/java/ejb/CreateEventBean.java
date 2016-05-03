dcb03ce76d86db5615e898e495c5675007c94dda
ragutis.lt <senelislt@hotmail.com>
2016-05-03 19:11:23 +0300

    merge

---
+++ src/main/java/ejb/CreateEventBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author Aurimas Dainius
+ */
+@Stateful
+public class CreateEventBean {
+
+    public void createEvent() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
--- src/main/java/ejb/FirstBean.java
+++ src/main/java/ejb/FirstBean.java
@@ -5,13 +5,10 @@
  */
 package ejb;
 
-import lt.edukuokis.entities.*;
-import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,24 +17,14 @@ import javax.persistence.Query;
 @Named
 //@RequestScoped // @SessionScoped
 @Stateless
-public class GetEventBean {
+public class FirstBean {
     @PersistenceContext
     private EntityManager em;
-    
-    private final List<Event> events = new ArrayList<>();
-    
-    @Inject
-    private CreateEventBean createEventBean;
-
-    public List<Event> getEvents() {
-        return events;
-    }
     
     public String ShowEvents() {
         Query all = em.createQuery("SELECT e FROM Event e");
         List results = all.getResultList();
-        return createEventBean.getClass().getName();
-        //return results.toString();
+        return results.toString();
         //return "dqdqdqd";
     }
     
